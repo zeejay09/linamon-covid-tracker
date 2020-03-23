6a54f4881d1e84b333802fd33fee15a01625a244
zeejay09 <zeejaybelamide@gmail.com>
2020-03-23 09:32:25 +0800

    adds dummy information

---
+++ src/containers/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Contact.css';
+
+export default class Contact extends Component {
+
+    render() {
+        return (
+            <div className="contact">
+                <div className="contact-info">
+                    <h2 className="contact-title-wrapper"><span className="contact-title">Linamon COVID-19 Tracker</span></h2>
+                </div>
+            </div>
+        )
+    }
+}
