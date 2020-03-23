1c60101469a84912937338b1051e4b4bc385dd2e
zeejay09 <zeejaybelamide@gmail.com>
2020-03-23 12:44:57 +0800

    adds user buttons

--- src/components/Home/InfoCard.js
+++ src/components/Home/InfoCard.js
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
 import Card from 'react-bootstrap/Card';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 class InfoCard extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = { 
+            pui: 0,
+            pum: 0,
+            isLoggedIn: sessionStorage.getItem('isLoggedIn') ? sessionStorage.getItem('isLoggedIn') : false,
+        };
+    }
+
     componentDidMount() {
-        axios.get('http://la-covid-api.jeexpoy.com/v1/puis/barangays/' + this.props.obj.id).then(response => {
+        axios.get('/puis/barangays/' + this.props.obj.id).then(response => {
             this.setState({ brgy: response.data.data });
             console.log(response.data.data);
         }).catch(function (error) {
@@ -17,10 +27,21 @@ class InfoCard extends Component {
 
         return (
             <Card>
-                <Card.Header className="text-center">{ this.props.obj.brgy_name }</Card.Header>
+                {this.state.isLoggedIn ?
+                    <>
+                        <Card.Header className="text-center">
+                            { this.props.obj.brgy_name }
+                            <Link to={ "/view/brgy/" + this.props.obj.id } className="btn btn-primary float-right">View</Link>
+                        </Card.Header>
+                    </>
+                :
+                    <>
+                        <Card.Header className="text-center">{ this.props.obj.brgy_name }</Card.Header>
+                    </>
+                }
                 <Card.Body>
                     <Card.Text>
-                        <span className="float-left">PUI: 0</span><span className="float-right">PUM: 0</span>
+                        <span className="float-left">PUI: { this.state.pui }</span><span className="float-right">PUM: { this.state.pum }</span>
                     </Card.Text>
                 </Card.Body>
             </Card>
