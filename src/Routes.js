c4eaa5e7f89f892903d3b7804e4838dd55adcc73
zeejay09 <zeejaybelamide@gmail.com>
2020-03-23 09:31:38 +0800

    adds route for login

--- src/Routes.js
+++ src/Routes.js
@@ -7,12 +7,16 @@ import Edit from './components/Edit';
 import View from './components/View';
 import Home from './containers/Home';
 import About from './containers/About';
+import Contact from './containers/Contact';
+import LoginForm from './components/Login/LoginForm';
 
 export default function Routes() {
   return (
     <Switch>
-        <Route path="/" exact component={ Home } />
+        <Route path='/' exact component={ Home } />
         <Route exact path='/about' component={ About } />
+        <Route exact path='/contact' component={ Contact } />
+        <Route exact path='/user' component={ LoginForm } />
         <Route exact path='/insert' component={ Insert } />
         <Route path='/edit/:id' component={ Edit } />
         <Route path='/view' component={ View } />
