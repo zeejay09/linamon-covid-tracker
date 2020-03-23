45c211ca527fd7f1acf4c44b45a67e0c6b433483
zeejay09 <zeejaybelamide@gmail.com>
2020-03-23 10:19:43 +0800

    changes from function to class

--- src/components/Navbar/NavigationBar.js
+++ src/components/Navbar/NavigationBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, NavLink } from 'react-bootstrap';
 import axios from 'axios';
@@ -6,9 +6,9 @@ import axios from 'axios';
 // My imports
 import './NavigationBar.css';
 
-function NavigationBar(props) {
+class NavigationBar extends Component {
 
-    const onSubmit = (e) => {
+    onSubmit = (e) => {
         e.preventDefault();
     
         const authToken = sessionStorage.getItem('authToken');
@@ -21,45 +21,47 @@ function NavigationBar(props) {
         );
     }
 
-    return (
-        <header>
-            <div className="container-fluid">
-                <Navbar bg="light" expand="lg">
-                    <NavLink href={'/'} className="navbar-brand">Municipality of Linamon</NavLink>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-                        <ul className="navbar-nav">
-                            {props.isLoggedIn ?
-                            <React.Fragment>
-                                <li className="nav-item">
-                                    <NavLink href='/about' className="nav-link">About</NavLink>
-                                </li>
-                                <li className="nav-item">
-                                    <NavLink href='/contact' className="nav-link">Contact</NavLink>
-                                </li>
-                                <form onSubmit={ onSubmit }>
+    render() {    
+        return (
+            <header>
+                <div className="container-fluid">
+                    <Navbar bg="light" expand="lg">
+                        <NavLink href={'/'} className="navbar-brand">Municipality of Linamon</NavLink>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+                            <ul className="navbar-nav">
+                                {this.props.isLoggedIn ?
+                                <React.Fragment>
                                     <li className="nav-item">
-                                    <button type="submit" className="nav-link bg-transparent">Logout</button>
+                                        <NavLink href='/about' className="nav-link">About</NavLink>
                                     </li>
-                                </form>
-                            </React.Fragment>
-                            :
-                            <React.Fragment>
-                                <li className="nav-item">
-                                    <NavLink href='/about' className="nav-link">About</NavLink>
-                                </li>
-                                <li className="nav-item">
-                                    <NavLink href='/contact' className="nav-link">Contact</NavLink>
-                                </li>
-                            </React.Fragment>
-                            }
-                        </ul>
-                    </Navbar.Collapse>
-                </Navbar>
-                
-            </div>
-        </header>
-    )
+                                    <li className="nav-item">
+                                        <NavLink href='/contact' className="nav-link">Contact</NavLink>
+                                    </li>
+                                    <form onSubmit={ this.onSubmit }>
+                                        <li className="nav-item">
+                                        <button type="submit" className="nav-link bg-transparent">Logout</button>
+                                        </li>
+                                    </form>
+                                </React.Fragment>
+                                :
+                                <React.Fragment>
+                                    <li className="nav-item">
+                                        <NavLink href='/about' className="nav-link">About</NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink href='/contact' className="nav-link">Contact</NavLink>
+                                    </li>
+                                </React.Fragment>
+                                }
+                            </ul>
+                        </Navbar.Collapse>
+                    </Navbar>
+                    
+                </div>
+            </header>
+        )
+    }
 }
 
 export default NavigationBar;
