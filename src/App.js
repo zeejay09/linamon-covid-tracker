a344b80c44e1a59f710afdf773bcbd85cdb015d8
zeejay09 <zeejaybelamide@gmail.com>
2020-03-23 10:19:05 +0800

    deletes commented code

--- src/App.js
+++ src/App.js
@@ -11,41 +11,13 @@ function App() {
 
   const isLoggedIn = sessionStorage.getItem('isLoggedIn') ? sessionStorage.getItem('isLoggedIn') : false;
 
-  
-    return (
-      <Router>
-        <NavigationBar {...{isLoggedIn}} />
+  return (
+    <Router>
+      <NavigationBar {...{isLoggedIn}} />
 
-        <Routes />
-      </Router>
-      )
-    // } else {
-    //   return (
-    //     <Router>
-    //       <header>
-    //         <div className="container-fluid">
-    //             <Navbar bg="light" expand="lg">
-    //                 <Link to={'/'} className="navbar-brand">Municipality of Linamon</Link>
-    //                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    //                 <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-    //                     <ul className="navbar-nav">
-    //                         <li className="nav-item">
-    //                             <Link to={'/about'} className="nav-link">About</Link>
-    //                         </li>
-    //                         <li className="nav-item">
-    //                             <Link to={'/contact'} className="nav-link">Contact</Link>
-    //                         </li>
-    //                     </ul>
-    //                 </Navbar.Collapse>
-    //             </Navbar>
-    
-    //             <Routes />
-                
-    //         </div>
-    //       </header>
-    //     </Router>
-    //   )
-    // }
+      <Routes />
+    </Router>
+    )
 }
 
 export default App;
