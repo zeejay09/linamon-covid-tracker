456fb927f64f638ffcf21b8a012e297dc3260efd
zeejay09 <zeejaybelamide@gmail.com>
2020-03-26 16:05:49 +0800

    changes api server to 1&1 without https

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import * as serviceWorker from './serviceWorker';
 import axios from 'axios';
 
 //Live Server 1 on 1
-axios.defaults.baseURL = 'https://la-covid-api.jeexpoy.com/v1';
+axios.defaults.baseURL = 'http://la-covid-api.jeexpoy.com/v1';
 
 // Live Server Heroku
 // axios.defaults.baseURL = 'https://la-covid-api.herokuapp.com/';
