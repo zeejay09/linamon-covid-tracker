56a0cf5e867e13670a52ebc44c3f28b3fa7777ac
zeejay09 <zeejaybelamide@gmail.com>
2020-04-06 15:57:03 +0800

    updates about section with useful links

--- src/containers/About.js
+++ src/containers/About.js
@@ -16,6 +16,22 @@ export default class About extends Component {
 
                     <p className="justify-content">The information from this website is directly coming from the authorized personnel of the Local Government Unit (LGU) of Linamon. The information presented may not be up-to-date from time to time because it is not automatically getting data, the data is manually inputted by an authorized user from the LGU Linamon.</p>
 
+                    <p className="mb-0">The following are useful links:</p>
+                    <ul>
+                        <li>
+                            Learn more about COVID-19 from WHO <a href="https://www.who.int/emergencies/diseases/novel-coronavirus-2019" target="_blank" rel="noopener noreferrer">here.</a>
+                        </li>
+                        <li>
+                            See DOH's latest updates <a href="https://www.doh.gov.ph/2019-nCoV" target="_blank" rel="noopener noreferrer">here.</a>
+                        </li>
+                        <li>
+                            <a href="http://www.covid19.gov.ph/covid-19-faqs/" target="_blank" rel="noopener noreferrer">COVID-19 FAQs</a>
+                        </li>
+                        <li>
+                            Live global <a href="https://www.worldometers.info/coronavirus/" target="_blank" rel="noopener noreferrer">tracker.</a>
+                        </li>
+                    </ul>
+
                     <div className="linamon-logo-wrapper mt-5">
                         <div className="linamon-logo">
                             <img src="/linamon-logo.png" alt="Municipality of Linamon" />
